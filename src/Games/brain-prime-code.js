4834a2099f5adc8ef04c76b8ab6646bac9f18ef2
имя фамилия <107165876+veganxjihad@users.noreply.github.com>
2023-08-04 18:28:00 +0500

    last version

--- src/Games/brain-prime-code.js
+++ src/Games/brain-prime-code.js
@@ -10,9 +10,9 @@ const isPrime = (num) => {
   if (num <= 1) {
     return false;
   }
-  for (let i = 2; i <= Math.sqrt(num); i+=1) {
-      if (num % i === 0) {
-      return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+    return false;
     }
   }
   return true;
