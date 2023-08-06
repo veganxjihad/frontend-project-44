576507d107af44651decad5effe913b431de3260
имя фамилия <107165876+veganxjihad@users.noreply.github.com>
2023-08-07 00:33:39 +0500

    last update

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,1 @@
-const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-export { getRandomNum };
+export const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
